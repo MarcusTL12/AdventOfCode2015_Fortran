0613b900be5371e38e721499421b38f22c3184ab
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-05-13 21:09:46 +0200

    Day21

--- src/day21.f90
+++ src/day21.f90
@@ -7,6 +7,8 @@ module day21_mod
     !
     private
     public :: part1, part2
+    !
+    integer, parameter :: boss(3) = (/103, 9, 2/)
 contains
     subroutine load_store(filename, wc, wd, ac, aa, rc, rd, ra)
         implicit none
@@ -72,17 +74,137 @@ contains
         close (1)
     end subroutine
     !
+    logical function playgame(player, boss) result(won)
+        implicit none
+        !
+        integer, intent(in) :: player(3), boss(3)
+        integer, target :: p(3), b(3)
+        integer, pointer :: a(:), d(:), tmp(:)
+        !
+        p = player
+        b = boss
+        !
+        a => p
+        d => b
+        !
+        won = .true.
+        !
+        do
+            if (doturn(a, d)) exit
+            tmp => a
+            a => d
+            d => tmp
+            won = .not. won
+        end do
+    contains
+        logical function doturn(attacker, defender) result(died)
+            implicit none
+            !
+            integer, intent(in) :: attacker(3)
+            integer, intent(inout) :: defender(3)
+            !
+            defender(1) = defender(1) - max(1, attacker(2) - defender(3))
+            died = defender(1) <= 0
+        end function
+    end function
+    !
     subroutine part1()
         implicit none
         !
         type(vec_int) :: wc, wd, ac, aa, rc, rd, ra
+        integer :: player(3), w, a, r1, r2, n, cost, ans
         !
-        call load_store("inputfiles/day21/shop.txt", wc, wd, ac, aa, rc, rd, ra)
+        call load_store("inputfiles/day21/shop.txt", &
+                        wc, wd, ac, aa, rc, rd, ra)
         !
+        n = 0
+        call ac%push(n)
+        call aa%push(n)
+        !
+        ans = 0
+        do w = 1, size(wc)
+            do a = 1, size(ac)
+                do r1 = 0, size(rc)
+                    if (r1 == 0) then
+                        n = 0
+                    else
+                        n = r1 + 1
+                    end if
+                    !
+                    do r2 = n, size(rc)
+                        player = (/100, wd%at(w), aa%at(a)/)
+                        cost = wc%at(w) + ac%at(a)
+                        if (r1 /= 0) then
+                            player(2) = player(2) + rd%at(r1)
+                            player(3) = player(3) + ra%at(r1)
+                            cost = cost + rc%at(r1)
+                        end if
+                        if (r2 /= 0) then
+                            player(2) = player(2) + rd%at(r2)
+                            player(3) = player(3) + ra%at(r2)
+                            cost = cost + rc%at(r2)
+                        end if
+                        !
+                        if (playgame(player, boss)) then
+                            if (ans == 0) then
+                                ans = cost
+                            else
+                                ans = min(ans, cost)
+                            end if
+                        end if
+                    end do
+                end do
+            end do
+        end do
+        !
+        print *, ans
     end subroutine
     !
     subroutine part2()
         implicit none
         !
+        type(vec_int) :: wc, wd, ac, aa, rc, rd, ra
+        integer :: player(3), w, a, r1, r2, n, cost, ans
+        !
+        call load_store("inputfiles/day21/shop.txt", &
+                        wc, wd, ac, aa, rc, rd, ra)
+        !
+        n = 0
+        call ac%push(n)
+        call aa%push(n)
+        !
+        ans = 0
+        do w = 1, size(wc)
+            do a = 1, size(ac)
+                do r1 = 0, size(rc)
+                    if (r1 == 0) then
+                        n = 0
+                    else
+                        n = r1 + 1
+                    end if
+                    !
+                    do r2 = n, size(rc)
+                        player = (/100, wd%at(w), aa%at(a)/)
+                        cost = wc%at(w) + ac%at(a)
+                        if (r1 /= 0) then
+                            player(2) = player(2) + rd%at(r1)
+                            player(3) = player(3) + ra%at(r1)
+                            cost = cost + rc%at(r1)
+                        end if
+                        if (r2 /= 0) then
+                            player(2) = player(2) + rd%at(r2)
+                            player(3) = player(3) + ra%at(r2)
+                            cost = cost + rc%at(r2)
+                        end if
+                        !
+                        if (.not. playgame(player, boss)) then
+                            ans = max(ans, cost)
+                        end if
+                    end do
+                end do
+            end do
+        end do
+        !
+        print *, ans
     end subroutine
 end module
